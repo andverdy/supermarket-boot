1b423baa19b6d6421e71febcb4a50109b7e18912
Andrea Verdiglione <andverdy@yahoo.it>
2022-08-28 18:43:05 +0200

    [EVO] change mapping api

--- src/main/java/it/objectmethod/supermarket/controller/ArticleController.java
+++ src/main/java/it/objectmethod/supermarket/controller/ArticleController.java
@@ -17,73 +17,74 @@ import it.objectmethod.supermarket.model.FamAssort;
 import it.objectmethod.supermarket.model.Iva;
 
 @Controller
+@RequestMapping("/articles")
 public class ArticleController {
 
-	@Autowired
-	private ArticleDao articleDao;
-	@Autowired
-	private IvaDao ivaDao;
-	@Autowired
-	private FamAssortDao famDao;
+    @Autowired
+    private ArticleDao articleDao;
+    @Autowired
+    private IvaDao ivaDao;
+    @Autowired
+    private FamAssortDao famDao;
 
-	@RequestMapping("/getArticles")
-	public String getArticles(ModelMap model) {
-		List<Article> listArticle = new ArrayList<Article>();
-		listArticle = articleDao.getArticles();
+    @RequestMapping("/get")
+    public String getArticles(ModelMap model) {
+        List<Article> listArticle = new ArrayList<Article>();
+        listArticle = articleDao.getArticles();
 
-		model.addAttribute("articleList", listArticle);
-		return "home";
+        model.addAttribute("articleList", listArticle);
+        return "home";
 
-	}
+    }
 
-	@RequestMapping("/formView")
-	public String formView(@RequestParam(value = "getCod", required = false) String getCod, ModelMap model) {
-		Article article = null;
+    @RequestMapping("/formView")
+    public String formView(@RequestParam(value = "getCod", required = false) String getCod, ModelMap model) {
+        Article article = null;
 
-		if (getCod != null) {
-			article = articleDao.getArticleByCode(getCod);
-		}
+        if (getCod != null) {
+            article = articleDao.getArticleByCode(getCod);
+        }
 
-		List<Iva> listIva;
-		List<FamAssort> listFamAssort;
+        List<Iva> listIva;
+        List<FamAssort> listFamAssort;
 
-		listIva = ivaDao.getIva();
-		listFamAssort = famDao.getFamAssort();
-		model.addAttribute("ivaList", listIva);
-		model.addAttribute("listFms", listFamAssort);
-		model.addAttribute("articleFormRefresh", article);
+        listIva = ivaDao.getIva();
+        listFamAssort = famDao.getFamAssort();
+        model.addAttribute("ivaList", listIva);
+        model.addAttribute("listFms", listFamAssort);
+        model.addAttribute("articleFormRefresh", article);
 
-		return "formIns";
-	}
+        return "formIns";
+    }
 
-	@RequestMapping("/insArt")
-	public String articleIns(@RequestParam("codice") String codArt, @RequestParam("descriz") String descr,
-			@RequestParam("pezziCart") Integer pzCart, @RequestParam("iva") Integer iva,
-			@RequestParam("fam") Integer fam, ModelMap model) {
-		int result = 0;
-		String message;
+    @RequestMapping("/save")
+    public String articleIns(@RequestParam("codice") String codArt, @RequestParam("descriz") String descr,
+                             @RequestParam("pezziCart") Integer pzCart, @RequestParam("iva") Integer iva,
+                             @RequestParam("fam") Integer fam, ModelMap model) {
+        int result = 0;
+        String message;
 
-		Article article = new Article();
-		article.setCodArt(codArt);
-		article.setDescrizione(descr);
-		article.setPzCart(pzCart);
-		article.setIdIva(iva);
-		article.setIdFamAss(fam);
+        Article article = new Article();
+        article.setCodArt(codArt);
+        article.setDescrizione(descr);
+        article.setPzCart(pzCart);
+        article.setIdIva(iva);
+        article.setIdFamAss(fam);
 
-		if (articleDao.getArticleByCode(article.getCodArt()) != null) {
-			result = articleDao.update(article);
-		} else {
-			result = articleDao.insert(article);
-		}
+        if (articleDao.getArticleByCode(article.getCodArt()) != null) {
+            result = articleDao.update(article);
+        } else {
+            result = articleDao.insert(article);
+        }
 
-		if (result == 1) {
-			message = "INSERIMENTO AVVENUTO CON SUCCESSO!";
-		} else {
-			message = "ERRORE INSERIMENTO!";
-		}
+        if (result == 1) {
+            message = "INSERIMENTO AVVENUTO CON SUCCESSO!";
+        } else {
+            message = "ERRORE INSERIMENTO!";
+        }
 
-		model.addAttribute("message", message);
-		return "forward:/getArticles";
-	}
+        model.addAttribute("message", message);
+        return "forward:/getArticles";
+    }
 
 }
